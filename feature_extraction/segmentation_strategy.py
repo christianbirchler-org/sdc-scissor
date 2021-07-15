18eb094a56cc95c649966b688a280de1d364cf94
Christian Birchler <christian.birchler2@uzh.ch>
2021-07-15 16:46:49 +0200

    specify return type

--- feature_extraction/segmentation_strategy.py
+++ feature_extraction/segmentation_strategy.py
@@ -2,5 +2,5 @@ import abc
 
 class SegmentationStrategy(abc.ABC):
     @abc.abstractmethod    
-    def extract_segments(self, road_points):
+    def extract_segments(self, road_points) -> list[tuple[int, int]]:
         pass
