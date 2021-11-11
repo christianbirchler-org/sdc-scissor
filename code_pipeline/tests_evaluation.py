bfd83640f87b0c99dd8e217aefdb9a1f16e6731c
Nicolas Ganz <gann@zhaw.ch>
2021-11-11 13:37:24 +0100

    removed trailing semicolons

--- code_pipeline/tests_evaluation.py
+++ code_pipeline/tests_evaluation.py
@@ -58,55 +58,55 @@ def _interpolate_and_resample_splines(sample_nodes, nodes_per_meter = 1, smoothn
 
 def _find_circle_and_return_the_center_and_the_radius(x1, y1, x2, y2, x3, y3):
     """THIS IS ONLY TO AVOID BREAKING OLD CODE"""
-    x12 = x1 - x2;
-    x13 = x1 - x3;
+    x12 = x1 - x2
+    x13 = x1 - x3
 
-    y12 = y1 - y2;
-    y13 = y1 - y3;
+    y12 = y1 - y2
+    y13 = y1 - y3
 
-    y31 = y3 - y1;
-    y21 = y2 - y1;
+    y31 = y3 - y1
+    y21 = y2 - y1
 
-    x31 = x3 - x1;
-    x21 = x2 - x1;
+    x31 = x3 - x1
+    x21 = x2 - x1
 
     # x1^2 - x3^2
-    sx13 = pow(x1, 2) - pow(x3, 2);
+    sx13 = pow(x1, 2) - pow(x3, 2)
 
     # y1^2 - y3^2
-    sy13 = pow(y1, 2) - pow(y3, 2);
+    sy13 = pow(y1, 2) - pow(y3, 2)
 
-    sx21 = pow(x2, 2) - pow(x1, 2);
-    sy21 = pow(y2, 2) - pow(y1, 2);
+    sx21 = pow(x2, 2) - pow(x1, 2)
+    sy21 = pow(y2, 2) - pow(y1, 2)
 
     f = (((sx13) * (x12) + (sy13) *
           (x12) + (sx21) * (x13) +
           (sy21) * (x13)) // (2 *
-                              ((y31) * (x12) - (y21) * (x13))));
+                              ((y31) * (x12) - (y21) * (x13))))
 
     g = (((sx13) * (y12) + (sy13) * (y12) +
           (sx21) * (y13) + (sy21) * (y13)) //
-         (2 * ((x31) * (y12) - (x21) * (y13))));
+         (2 * ((x31) * (y12) - (x21) * (y13))))
 
     c = (-pow(x1, 2) - pow(y1, 2) -
-         2 * g * x1 - 2 * f * y1);
+         2 * g * x1 - 2 * f * y1)
 
     # eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
     # where centre is (h = -g, k = -f) and
     # radius r as r^2 = h^2 + k^2 - c
-    h = -g;
-    k = -f;
-    sqr_of_r = h * h + k * k - c;
+    h = -g
+    k = -f
+    sqr_of_r = h * h + k * k - c
 
     # r is the radius
-    r = round(sqrt(sqr_of_r), 5);
+    r = round(sqrt(sqr_of_r), 5)
 
     return ((h, k), r)
 
 
-    #print("Centre = (", h, ", ", k, ")");
-    #print("Radius = ", r);
-    #print("Radius = ", degrees(r));
+    #print("Centre = (", h, ", ", k, ")")
+    #print("Radius = ", r)
+    #print("Radius = ", degrees(r))
 
 
 def _road_segments_grouper(iterable, radius_tolerance=0.3):
