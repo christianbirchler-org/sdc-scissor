6b67df58aa5b8fc65bc8eb13976f29ec251b2dee
Nicolas Ganz <gann@zhaw.ch>
2021-11-15 12:28:35 +0100

    fixed pylint and flake8 issues for feature_extraction

--- feature_extraction/angle_based_strategy.py
+++ feature_extraction/angle_based_strategy.py
@@ -1,6 +1,7 @@
 from feature_extraction.segmentation_strategy import SegmentationStrategy
 from feature_extraction.road_geometry_calculator import RoadGeometryCalculator
 
+
 class AngleBasedStrategy(SegmentationStrategy):
     """
     Define segments based on identified turns. The center of a turn where the
@@ -29,7 +30,8 @@ class AngleBasedStrategy(SegmentationStrategy):
         for i in range(len(road_points)):
 
             # check if it is the last iteration
-            if i == len(road_points)-1: is_last_iteration = True
+            if i == len(road_points)-1:
+                is_last_iteration = True
 
             # the start of a new piece has to be 2 indexes ahead of the current i
             if current_road_piece_start_index == i+1:
@@ -39,22 +41,23 @@ class AngleBasedStrategy(SegmentationStrategy):
 
             # define the current road piece to calculate the turn angle and distane
             current_road_piece = road_points[current_road_piece_start_index:current_road_piece_end_index+1]
-            
+
             # calculate the road piece distance defined above
             current_distance = self.__road_geometry_calculator.get_road_length(current_road_piece)
-            
+
             # check if the distance of the current road piece is long enough or it is the last iteration
             if (current_distance >= self.__decision_distance) or is_last_iteration:
                 previous_angle = current_angle
 
                 # calculate the angle of the current road piece
-                # TODO: ensure that the road piece to calculate the angle has enough points!!! (e.g., use temporarily a longer road piece)
+                # TODO: ensure that the road piece to calculate the angle has enough points!!!
+                # (e.g., use temporarily a longer road piece)
                 if len(current_road_piece) == 2 and current_road_piece_start_index > 0:
                     tmp_current_road_piece = road_points[current_road_piece_start_index-1:current_road_piece_end_index+1]
                 else:
                     tmp_current_road_piece = current_road_piece
                 current_angle = sum(self.__road_geometry_calculator.extract_turn_angles(tmp_current_road_piece))
-            
+
                 # define start and end index of segment iff the angle of the current road piece is different
                 if (self.__has_current_angle_changed(previous_angle, current_angle) and not is_first_piece) or is_last_iteration:
                     segment_end_index = i
@@ -69,7 +72,6 @@ class AngleBasedStrategy(SegmentationStrategy):
 
         # decide type of segment based on angle and its threshold
         # define segment when the type of segment will change
-    
 
     def __has_current_angle_changed(self, previous_angle, current_angle):
         angle_threshold = self.__angle_threshold
@@ -89,14 +91,13 @@ if __name__ == '__main__':
             center_of_turn = (50, 0)
 
             for i in range(0, 91, 1):
-                x = -1 * radius * math.cos(math.radians(i)) # minus for right turn
+                x = -1 * radius * math.cos(math.radians(i))  # minus for right turn
                 y = radius * math.sin(math.radians(i))
 
                 # translation of coordinates
                 x = x + center_of_turn[0]
 
                 road_points.append((x, y))
-
 
             strategy = AngleBasedStrategy(angle_threshold=5, decision_distance=10)
 
@@ -113,7 +114,7 @@ if __name__ == '__main__':
 
             # left turn
             for i in range(0, angle+1, 1):
-                x = radius * math.cos(math.radians(i)) # minus for right turn
+                x = radius * math.cos(math.radians(i))  # minus for right turn
                 y = radius * math.sin(math.radians(i))
 
                 # translation of coordinates
@@ -122,11 +123,10 @@ if __name__ == '__main__':
 
                 road_points.append((x, y))
 
-
             # right turn
             center_of_turn = (x, y+radius)
             for i in range(1, angle+1, 1):
-                x = -radius * math.sin(math.radians(i)) # minus for right turn
+                x = -radius * math.sin(math.radians(i))  # minus for right turn
                 y = -radius * math.cos(math.radians(i))
 
                 # translation of coordinates
@@ -172,7 +172,7 @@ if __name__ == '__main__':
 
             # left turn
             for i in range(0, angle+1, 1):
-                x = radius * math.cos(math.radians(i)) # minus for right turn
+                x = radius * math.cos(math.radians(i))  # minus for right turn
                 y = radius * math.sin(math.radians(i))
 
                 # translation of coordinates
@@ -199,12 +199,22 @@ if __name__ == '__main__':
             self.assertAlmostEqual(first_segment_end_index, 90, places=None, delta=5)
             self.assertAlmostEqual(second_segment_start_index, 91, places=None, delta=5)
 
-
-
-
-        def test_frenetic_generated_road(self):
-
-            road_points = [(66.38752953287502, 78.01492758476358), (66.39549279998235, 88.01492441408193), (63.41488386330962, 97.56039299171744), (59.39849432596085, 106.71837394192012), (53.717347876135484, 114.94786815576077), (45.71861092315158, 120.94955184927294), (35.923362286475445, 122.96278411249901), (26.205075299578844, 120.60590099271641), (17.811834039492837, 115.1695877980973), (12.078084922014867, 106.97665663411084), (10.0, 97.19496132824005), (13.48150774832019, 87.82057581246428), (20.764260856833666, 80.96772916596494), (29.79153825268792, 76.66560610655593), (39.428935001131215, 73.99716388063186), (48.20671802392773, 69.20649323815337), (55.656007423636034, 62.53505505441866), (62.62894703718282, 55.3672022974328), (69.3230484201203, 47.93827191177469), (74.39518316897082, 39.320067395200304), (75.92736990007486, 29.438144312063663), (74.25081132886308, 19.579688481798136), (71.38208886203154, 10.0)]
+        @staticmethod
+        def test_frenetic_generated_road():
+            road_points = [
+                (66.38752953287502, 78.01492758476358), (66.39549279998235, 88.01492441408193),
+                (63.41488386330962, 97.56039299171744), (59.39849432596085, 106.71837394192012),
+                (53.717347876135484, 114.94786815576077), (45.71861092315158, 120.94955184927294),
+                (35.923362286475445, 122.96278411249901), (26.205075299578844, 120.60590099271641),
+                (17.811834039492837, 115.1695877980973), (12.078084922014867, 106.97665663411084),
+                (10.0, 97.19496132824005), (13.48150774832019, 87.82057581246428),
+                (20.764260856833666, 80.96772916596494), (29.79153825268792, 76.66560610655593),
+                (39.428935001131215, 73.99716388063186), (48.20671802392773, 69.20649323815337),
+                (55.656007423636034, 62.53505505441866), (62.62894703718282, 55.3672022974328),
+                (69.3230484201203, 47.93827191177469), (74.39518316897082, 39.320067395200304),
+                (75.92736990007486, 29.438144312063663), (74.25081132886308, 19.579688481798136),
+                (71.38208886203154, 10.0),
+            ]
 
             strategy = AngleBasedStrategy(angle_threshold=5, decision_distance=10)
 
@@ -214,13 +224,12 @@ if __name__ == '__main__':
 
         def test_angle_based_segmentation(self):
 
-            road_points = [(20,20), (50,30), (70,50), (80,80), (70,110), (50,130), (50,150), (60,160), (80,170), (100,170), (120,160), (140,120), (150,80), (150,50), (150,30), (150,20)]
+            road_points = [(20, 20), (50, 30), (70, 50), (80, 80), (70, 110), (50, 130), (50, 150), (60, 160), (80, 170),
+                           (100, 170), (120, 160), (140, 120), (150, 80), (150, 50), (150, 30), (150, 20)]
 
             segmentation_strategy = AngleBasedStrategy(angle_threshold=10, decision_distance=10)
-            segments = segmentation_strategy.extract_segments(road_points)
-            
-            self.assertTrue(False)
+            segmentation_strategy.extract_segments(road_points)
 
-
+            self.assertTrue(False)  # pylint: disable=redundant-unittest-assert
 
     unittest.main()
--- feature_extraction/equi_distance_strategy.py
+++ feature_extraction/equi_distance_strategy.py
@@ -1,5 +1,6 @@
 from feature_extraction.segmentation_strategy import SegmentationStrategy
 
+
 class EquiDistanceStrategy(SegmentationStrategy):
     """
     Define segments with equal lengts. The last segment might be different to
@@ -34,7 +35,7 @@ class EquiDistanceStrategy(SegmentationStrategy):
                 end = len(road_points) - 1
             else:
                 end = start + road_points_per_segment - 1
-            
+
             current_segment = (start, end)
             segments.append(current_segment)
 
@@ -52,13 +53,13 @@ if __name__ == "__main__":
             nr_of_segments = 5
             strategy = EquiDistanceStrategy(nr_of_segments)
 
-            road_points = [(1,1),(2,1),(3,1),(4,1),(5,1),(6,1)]
+            road_points = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1)]
 
             segments = strategy.extract_segments(road_points)
 
             self.assertEqual(len(segments), 5, "There must be {} segments.".format(nr_of_segments))
 
-            expected_segments = [(0,1),(1,2),(2,3),(3,4),(4,5)]
+            expected_segments = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
             self.assertEqual(segments, expected_segments, "Segments are not correctly calculated.")
 
         def test_too_few_road_points_should_raise_exception(self):
@@ -66,7 +67,7 @@ if __name__ == "__main__":
             strategy = EquiDistanceStrategy(nr_of_segments)
 
             # 6 road points
-            road_points = [(1,1),(2,1),(3,1),(4,1),(5,1),(6,1)]
+            road_points = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1)]
 
             self.assertRaises(Exception, strategy.extract_segments(road_points))
 
@@ -74,24 +75,22 @@ if __name__ == "__main__":
             nr_of_segments = 4
             strategy = EquiDistanceStrategy(nr_of_segments)
 
-            road_points = [(1,1),(2,1),(3,1),(4,1),(5,1),(6,1)]
+            road_points = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1)]
 
             segments = strategy.extract_segments(road_points)
 
-            expected_segments = [(0,1),(1,2),(2,3),(3,5)]
+            expected_segments = [(0, 1), (1, 2), (2, 3), (3, 5)]
             self.assertEqual(segments, expected_segments, "Segments are not correctly calculated.")
-
 
         def test_last_segment_has_less_road_points_than_others(self):
             nr_of_segments = 3
             strategy = EquiDistanceStrategy(nr_of_segments)
 
-            road_points = [(1,1),(2,1),(3,1),(4,1),(5,1),(6,1)]
+            road_points = [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1)]
 
             segments = strategy.extract_segments(road_points)
 
-            expected_segments = [(0,2),(2,4),(4,5)]
+            expected_segments = [(0, 2), (2, 4), (4, 5)]
             self.assertEqual(segments, expected_segments, "Segments are not correctly calculated.")
 
     unittest.main()
-    
