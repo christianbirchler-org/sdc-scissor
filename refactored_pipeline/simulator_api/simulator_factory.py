bbe44d89742beaabf6a8add1a681abfd2892f1a5
Christian Birchler <christian.birchler2@uzh.ch>
2022-03-23 16:32:07 +0100

    #15 Add basic skeleton for the simulator factory component
    Already added a sample code for BeamNG

---
+++ refactored_pipeline/simulator_api/simulator_factory.py
@@ -0,0 +1,14 @@
+from beamngpy import BeamNGpy
+
+
+class SimulatorFactory:
+    @staticmethod
+    def get_beamng_simulator() -> BeamNGpy:
+        beamng = BeamNGpy('localhost', 64256,
+                          home=r'C:\Users\birc\Documents\BeamNG.tech.v0.24.0.2\BeamNG.drive-0.24.0.2.13392',
+                          user=r'C:\Users\birc\Documents\BeamNG.drive')
+        return beamng
+
+
+if __name__ == '__main__':
+    print('* simulator_factory.py')
