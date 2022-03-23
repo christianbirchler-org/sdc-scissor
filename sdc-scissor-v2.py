09ee020db321f7ab31b1f49466a29cc9cc223459
Christian Birchler <christian.birchler2@uzh.ch>
2022-03-23 16:48:26 +0100

    #15 Simple test with new BeamNG.tech version

--- sdc-scissor-v2.py
+++ sdc-scissor-v2.py
@@ -1,6 +1,8 @@
 import click
 
-from refactored_pipeline.main import main as new_main
+from refactored_pipeline.testing_api.test import Test
+from refactored_pipeline.simulator_api.simulator_factory import SimulatorFactory
+from refactored_pipeline.testing_api.test_runner import TestRunner
 
 
 @click.group()
@@ -30,7 +32,11 @@ def predict_tests():
 
 @cli.command()
 def refactored_pipeline():
-    new_main()
+    road_points = [(0, 0, -28, 10), (30, -30, -28, 10), (30, -100, -28, 10), (0, -200, -28, 10)]
+    test = Test(road_points=road_points)
+    beamng = SimulatorFactory.get_beamng_simulator()
+    test_runner = TestRunner(simulator=beamng)
+    test_runner.run(test)
 
 
 if __name__ == '__main__':
