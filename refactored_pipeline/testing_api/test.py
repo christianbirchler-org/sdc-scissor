ccafded25f49edb7271a4bc221c2291284f115a5
Christian Birchler <christian.birchler2@uzh.ch>
2022-03-23 16:34:53 +0100

    #15 Add skeletons for handling Test objects

---
+++ refactored_pipeline/testing_api/test.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Test:
+    road_points: list[tuple]
+
+
+if __name__ == '__main__':
+    print('* test.py')
---
+++ refactored_pipeline/testing_api/test_loader.py
@@ -0,0 +1,6 @@
+class TestLoader:
+    pass
+
+
+if __name__ == '__main__':
+    print('* test_loader.py')
