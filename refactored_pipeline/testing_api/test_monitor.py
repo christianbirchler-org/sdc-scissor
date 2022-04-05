96822a763322b30922fe2d505e3f4f52edf7512c
Christian Birchler <christian.birchler2@uzh.ch>
2022-04-05 11:57:46 +0200

    #15 Add skeleton for checking if car is at the end of the road

--- refactored_pipeline/testing_api/test_monitor.py
+++ refactored_pipeline/testing_api/test_monitor.py
@@ -8,7 +8,7 @@ class TestMonitor:
         self.simulator = simulator
         self.vehicle = vehicle
         self.test = test
-        self.is_finished = False
+        self.is_car_at_end_of_road = False
         self.car_is_out_of_lane = False
         self.road = None
 
@@ -20,6 +20,19 @@ class TestMonitor:
         x_pos = state['pos'][0]
         y_pos = state['pos'][1]
         print('** x: {}\ny: {}\n\n'.format(x_pos, y_pos))
+        if self.__car_at_end_of_road(x_pos, y_pos):
+            self.is_car_at_end_of_road = True
+
+    def __car_at_end_of_road(self, x_pos: float, y_pos: float) -> bool:
+        print('* __car_at_end_of_road')
+        road_end_point = self.test.interpolated_points[-1]
+        x_end, y_end = road_end_point[0], road_end_point[1]
+        res: bool = self.__are_points_close((x_pos, y_pos), (x_end, y_end), 2)
+        return res
+
+    @staticmethod
+    def __are_points_close(a: tuple[float, float], b: tuple[float, float], threshold: float):
+        return False
 
 
 if __name__ == '__main__':
