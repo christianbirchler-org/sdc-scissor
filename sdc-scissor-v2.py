eebe9139677c975d68cad581bf5e46a9b999731d
Christian Birchler <christian.birchler2@uzh.ch>
2022-04-08 14:44:38 +0200

    #15 bugfix `refactored-pipeline` command

--- sdc-scissor-v2.py
+++ sdc-scissor-v2.py
@@ -67,9 +67,10 @@ def predict_tests():
 def refactored_pipeline():
     road_points = [[0, 0, -28, 10], [30, -30, -28, 10], [30, -100, -28, 10], [0, -200, -28, 10]]
     test = Test(road_points=road_points)
-    beamng = SimulatorFactory.get_beamng_simulator()
-    test_runner = TestRunner(simulator=beamng)
-    test_runner.run_test_suite(test)
+    simulator = SimulatorFactory.get_beamng_simulator()
+    test_runner = TestRunner(simulator=simulator)
+    simulator.open()
+    test_runner.run(test)
 
 
 if __name__ == '__main__':
