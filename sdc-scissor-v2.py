2f709080fca30a49c8a2525af70673d109a11cfc
Christian Birchler <christian.birchler2@uzh.ch>
2022-04-11 12:31:28 +0200

    #15 data loading

--- sdc-scissor-v2.py
+++ sdc-scissor-v2.py
@@ -10,7 +10,7 @@ from refactored_pipeline.testing_api.test_runner import TestRunner
 from refactored_pipeline.testing_api.test_generator import TestGenerator
 from refactored_pipeline.testing_api.test_loader import TestLoader
 from refactored_pipeline.feature_extraction_api.feature_extraction import FeatureExtractor
-from refactored_pipeline.feature_extraction_api.angle_based_strategy import AngleBasedStrategy
+from refactored_pipeline.machine_learning_api.csv_loader import CSVLoader
 
 
 @click.group()
@@ -72,8 +72,79 @@ def label_tests(tests):
 
 
 @cli.command()
-def evaluate_models():
-    pass
+@click.option('--csv', default='./destination/road_features.csv', type=click.Path(exists=True))
+def evaluate_models(csv):
+    """
+    Evaluate different machine learning models.
+    """
+    logging.info('evaluate_models')
+    data_path = Path(csv)
+    dd = CSVLoader.load_dataframe_from_csv(data_path)
+    logging.info(dd)
+    ###############################################################
+    ############################ OLD CODE #########################
+    ###############################################################
+    # logging.info('evaluate_models')
+    # abs_path = os.path.abspath(tests)
+    # df, _ = load_data_as_data_frame(abs_path)
+    #
+    # # consider only data we know before the execution of the scenario
+    # X_attributes = ['direct_distance', 'max_angle',
+    #                 'max_pivot_off', 'mean_angle', 'mean_pivot_off', 'median_angle',
+    #                 'median_pivot_off', 'min_angle', 'min_pivot_off', 'num_l_turns',
+    #                 'num_r_turns', 'num_straights', 'road_distance', 'std_angle',
+    #                 'std_pivot_off', 'total_angle']
+    #
+    # y_attribute = 'safety'
+    #
+    # # train models CV
+    # X = df[X_attributes].to_numpy()
+    # # TODO: provide preprocessing options to the user???
+    # # X = preprocessing.normalize(X)
+    # # X = preprocessing.scale(X)
+    # y = df[y_attribute].to_numpy()
+    # y[y == 'FAIL'] = 1
+    # y[y == 'PASS'] = 0
+    # y = np.array(y, dtype='int32')
+    #
+    # classifiers = {}
+    # scoring = ['accuracy', 'precision', 'recall', 'f1']
+    #
+    # classifiers = {
+    #     'random_forest': RandomForestClassifier(),
+    #     'gradient_boosting': GradientBoostingClassifier(),
+    #     # 'multinomial_naive_bayes': MultinomialNB(),
+    #     'gaussian_naive_bayes': GaussianNB(),
+    #     'logistic_regression': LogisticRegression(max_iter=10000),
+    #     'decision_tree': DecisionTreeClassifier(),
+    # }
+    #
+    # for name, estimator in classifiers.items():
+    #     scores = cross_validate(estimator, X, y, cv=KFold(n_splits=10, shuffle=True), scoring=scoring)
+    #     classifiers[name] = {
+    #         'estimator': estimator,
+    #         'scores': scores,
+    #         # average the scores
+    #         'avg_scores': get_avg_scores(scores),
+    #     }
+    #
+    # # output results
+    # for key, value in classifiers.items():
+    #     avg_scores = value['avg_scores']
+    #     model = key
+    #     accuracy = avg_scores['accuracy']
+    #     recall = avg_scores['recall']
+    #     precision = avg_scores['precision']
+    #     f1 = avg_scores['f1']
+    #
+    #     if save:
+    #         model_file_name = key + '.joblib'
+    #         trained_model = value['estimator'].fit(X, y)
+    #         joblib.dump(trained_model, model_file_name)
+    #
+    #     # TODO: order the models according to an argument (e.g. acc, rec, prec, f1)
+    #     print('MODEL: {:<25} ACCURACY: {:<20} RECALL: {:<20} PRECISION: {:<20} F1: {}'
+    #           .format(model, accuracy, recall, precision, f1))
 
 
 @cli.command()
