b6d92a6cb985a9fec62197c71221036ac35497d8
Christian Birchler <christian.birchler2@uzh.ch>
2022-04-13 14:18:09 +0200

    #15 code for cost-effectiveness

--- sdc-scissor-v2.py
+++ sdc-scissor-v2.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import KFold, cross_validate, train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import metrics
+from sklearn.metrics import accuracy_score, recall_score, f1_score, precision_score
 
 from refactored_pipeline.testing_api.test import Test
 from refactored_pipeline.simulator_api.simulator_factory import SimulatorFactory
@@ -114,14 +114,40 @@ def evaluate_models(csv, models_dir):
     label = 'safety'
     duration_attr = 'duration'
 
+    dd = dd.sample(frac=1).reset_index(drop=True)
+    N = dd.shape[0]
+    N_train = int(N*0.8)
+    N_test = N - N_train
+
     X = dd[road_features].to_numpy()
+    X_train = X[0:N_train, :]
+    X_test = X[N_train:, :]
     X = preprocessing.normalize(X)
     X = preprocessing.scale(X)
-    y = dd[label].to_numpy()
 
+    y = dd[label].to_numpy()
     y[y == 'FAIL'] = 1
     y[y == 'PASS'] = 0
     y = np.array(y, dtype='int32')
+
+    y_train = y[:N_train]
+
+    X_train_pass = X_train[y_train == 0, :]
+    X_train_fail = X_train[y_train == 1, :]
+    y_train_pass = y_train[y_train == 0]
+    y_train_fail = y_train[y_train == 1]
+    y_test = y[N_train:]
+
+    print(y_train_pass)
+
+    n_fail = len(y_train_fail)
+    n_pass = len(y_train_pass)
+    if n_fail < n_pass:
+        y_train = np.concatenate((y_train_fail, y_train_pass[:n_fail]))
+        X_train = np.concatenate((X_train_fail, X_train_pass[:n_fail, :]), axis=0)
+    else:
+        y_train = np.concatenate((y_train_pass, y_train_fail[:n_pass]))
+        X_train = np.concatenate((X_train_pass, X_train_fail[:n_pass, :]), axis=0)
 
     scoring = ['accuracy', 'precision', 'recall', 'f1']
 
@@ -133,29 +159,97 @@ def evaluate_models(csv, models_dir):
         'decision_tree': DecisionTreeClassifier(),
     }
 
-    for name, estimator in classifiers.items():
-        scores = cross_validate(estimator, X, y, cv=KFold(n_splits=10, shuffle=True), scoring=scoring)
-        classifiers[name] = {
-            'estimator': estimator,
-            'scores': scores,
-            'avg_scores': get_avg_scores(scores)
-        }
-
-    for key, value in classifiers.items():
-        avg_scores = value['avg_scores']
-        model = key
-        accuracy = avg_scores['accuracy']
-        recall = avg_scores['recall']
-        precision = avg_scores['precision']
-        f1 = avg_scores['f1']
-
-        model_file_name = key + '.joblib'
-        model_file_path = models_path / model_file_name
-        trained_model = value['estimator'].fit(X, y)
-        joblib.dump(trained_model, model_file_path)
-
+    file_path = None
+    for model_name, model in classifiers.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        prec = precision_score(y_test, y_pred)
+        rec = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+        filename = model_name + '.joblib'
+        file_path = models_path / filename
+        logging.info('save model: {}'.format(model_name))
+        joblib.dump(model, file_path)
         print('MODEL: {:<25} ACCURACY: {:<20} RECALL: {:<20} PRECISION: {:<20} F1: {}'
-              .format(model, accuracy, recall, precision, f1))
+              .format(model_name, acc, rec, prec, f1))
+
+
+@cli.command()
+@click.option('--csv', help='Path to labeled tests', type=click.Path(exists=True))
+@click.option('--train-ratio', default=0.7, help='Ratio used for training the models', type=click.FLOAT)
+def evaluate_cost_effectiveness(csv, train_ratio):
+    logging.info('evaluate_cost_effectiveness')
+    data_path = Path(csv)
+    dd = CSVLoader.load_dataframe_from_csv(data_path)
+    X_model_attributes = ['direct_distance', 'max_angle', 'max_pivot_off', 'mean_angle',
+                          'mean_pivot_off', 'median_angle', 'median_pivot_off', 'min_angle', 'min_pivot_off',
+                          'num_l_turns', 'num_r_turns', 'num_straights', 'road_distance', 'std_angle', 'std_pivot_off',
+                          'total_angle']
+    y_attribute = 'safety'
+    time_attribute = 'duration'
+
+    df = dd.sample(frac=1).reset_index(drop=True)
+
+
+
+    X_attributes = X_model_attributes + [time_attribute]
+
+    # train models CV
+    X = df[X_attributes].to_numpy()
+    # print(X)
+    # print(X[:, -1])
+    # TODO: provide preprocessing options to the user???
+    # X = preprocessing.normalize(X)
+    # X = preprocessing.scale(X)
+    y = df[y_attribute].to_numpy()
+    y[y == 'FAIL'] = 1
+    y[y == 'PASS'] = 0
+    y = np.array(y, dtype='int32')
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_ratio)
+
+    X_test_time = X_test[:, -1]
+
+    X_train = X_train[:, :-1]
+    X_test = X_test[:, :-1]
+
+    classifiers = {}
+
+    classifiers = {
+        'random_forest': RandomForestClassifier(),
+        'gradient_boosting': GradientBoostingClassifier(),
+        # 'multinomial_naive_bayes': MultinomialNB(),
+        'gaussian_naive_bayes': GaussianNB(),
+        'logistic_regression': LogisticRegression(max_iter=10000),
+        'decision_tree': DecisionTreeClassifier(),
+    }
+
+    nr_unsafe_predicted = None
+    for name, estimator in classifiers.items():
+        estimator.fit(X_train, y_train)
+        y_pred = estimator.predict(X_test)
+        nr_unsafe_predicted = np.sum(y_pred)
+
+        rand_sim_times = []
+        nr_trials = 10
+        for i in range(nr_trials):
+            random_unsafe_predicted = np.random.permutation(np.append(np.ones(nr_unsafe_predicted, dtype='int32'),
+                                                                      np.zeros(len(y_pred - nr_unsafe_predicted),
+                                                                               dtype='int32')))
+            rand_sim_times.append(np.sum(X_test_time[random_unsafe_predicted]))
+
+        random_tot_sim_time = np.mean(rand_sim_times)
+
+        sdc_scissor_tot_sim_time = np.sum(X_test_time[y_pred])
+        print('SDC-SCISSOR')
+        print('{}:\tnr_tests: {}\ttot_sim_time {}'.
+              format(name, nr_unsafe_predicted, sdc_scissor_tot_sim_time))
+        print('RANDOM BASELINE:')
+        print('nr_tests: {}\ttot_sim_time {}'.
+              format(nr_unsafe_predicted, random_tot_sim_time))
+        print('random_baseline_time/sdc_scissor_time = {}'.format(random_tot_sim_time / sdc_scissor_tot_sim_time))
+        print('sdc_scissor_time/random_baseline_time = {}\n'.format(sdc_scissor_tot_sim_time/random_tot_sim_time))
 
 
 @cli.command()
