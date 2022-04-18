32be8cbc8030433f09859ff2bb637a3a31d4fc19
Christian Birchler <christian.birchler2@uzh.ch>
2022-04-18 19:15:47 +0200

    #15 use geometric set functions to decide if car is off the road

--- sdc_scissor/testing_api/test_monitor.py
+++ sdc_scissor/testing_api/test_monitor.py
@@ -7,6 +7,13 @@ from sdc_scissor.testing_api.test import Test
 from sdc_scissor.simulator_api.abstract_simulator import AbstractSimulator
 
 
+class _RoadModel:
+    def __init__(self, road_points: list[list]):
+        self.coordinates = [(x[0], x[1]) for x in road_points]
+        self.center_line: LineString = LineString(coordinates=self.coordinates)
+        self.right_lane = self.center_line.buffer(distance=-5, single_sided=True)
+
+
 class TestMonitor:
     def __init__(self, simulator: AbstractSimulator, test: Test):
         self.simulator = simulator
@@ -17,6 +24,7 @@ class TestMonitor:
         self.start_time = None
         self.end_time = None
         self.data = []
+        self.road_model = _RoadModel(test.interpolated_road_points)
 
     def check(self):
         logging.info('check')
@@ -42,6 +50,15 @@ class TestMonitor:
     def dump_data(self):
         logging.info('dump_data')
         # TODO
+
+    def __is_car_out_of_lane(self, x_pos: float, y_pos: float) -> bool:
+        # TODO: Issue #22
+        logging.info('__is_car_out_of_lane')
+        car_model = box(x_pos-1, y_pos-1, x_pos+1, y_pos+1)
+        is_car_model_inside_road_model = self.road_model.right_lane.covers(car_model)
+        if not is_car_model_inside_road_model:
+            logging.warning('CAR IS OFF THE LANE!')
+        return not is_car_model_inside_road_model
 
     def __is_car_at_end_of_road(self, x_pos: float, y_pos: float) -> bool:
         logging.info('__car_at_end_of_road')
