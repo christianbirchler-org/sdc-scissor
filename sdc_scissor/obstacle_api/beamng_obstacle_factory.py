5ffebc45395c8dfeef284f2e7e69f30fb051cf59
Christian Birchler <christian.birchler2@uzh.ch>
2022-06-01 21:09:01 +0200

    #39 add skeleton code for abstract factory pattern

---
+++ sdc_scissor/obstacle_api/beamng_obstacle_factory.py
@@ -0,0 +1,17 @@
+import logging
+
+from sdc_scissor.obstacle_api.bump import Bump
+from sdc_scissor.obstacle_api.delineator import Delineator
+from sdc_scissor.obstacle_api.obstacle_factory import ObstacleFactory
+
+
+class BeamngObstacleFactory(ObstacleFactory):
+    def create_bump(self) -> Bump:
+        pass
+
+    def create_delineator(self) -> Delineator:
+        pass
+
+
+if __name__ == '__main__':
+    logging.info('beamng_obstacle_factory.py')
--- sdc_scissor/obstacle_api/obstacle_factory.py
+++ sdc_scissor/obstacle_api/obstacle_factory.py
@@ -1,9 +1,18 @@
 import logging
 import abc
 
+from sdc_scissor.obstacle_api.bump import Bump
+from sdc_scissor.obstacle_api.delineator import Delineator
+
 
 class ObstacleFactory(abc.ABC):
-    pass
+    @abc.abstractmethod
+    def create_bump(self) -> Bump:
+        pass
+
+    @abc.abstractmethod
+    def create_delineator(self) -> Delineator:
+        pass
 
 
 if __name__ == '__main__':
