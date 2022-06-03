0bf889bee4af659ee52331aa151b86ea7d488483
Christian Birchler <christian.birchler2@uzh.ch>
2022-06-03 16:28:17 +0200

    #42 Implement test cases

---
+++ tests/sdc_scissor/testing_api/test_test_monitor.py
@@ -0,0 +1,30 @@
+import logging
+
+from sdc_scissor.testing_api.test_monitor import TestMonitor
+
+
+class TestTestMonitor:
+    def setup_class(self):
+        pass
+
+    def teardown_class(self):
+        pass
+
+    def setup_method(self):
+        self.test_monitor = TestMonitor(simulator=None, test=None, oob=None, road_model=None)
+
+    def test_is_car_moving_car_stays_on_same_position(self):
+        self.test_monitor.data = [(0, 0, 0, 0), (5, 0, 0, 0)]
+        expected = False
+        actual = self.test_monitor.is_car_moving()
+        assert expected == actual
+
+    def test_is_car_moving_car_stays_not_on_same_position(self):
+        self.test_monitor.data = [(0, 0, 0, 0), (5, 10, 10, 0)]
+        expected = True
+        actual = self.test_monitor.is_car_moving()
+        assert expected == actual
+
+
+if __name__ == '__main__':
+    logging.info('test_test_monitor.py')
