0211ff08c7d51b3807d6516bc527ceeb181addbe
Christian Birchler <christian.birchler2@uzh.ch>
2022-07-01 12:54:20 +0200

    #48 adapt test cases

--- tests/sdc_scissor/testing_api/test_test_monitor.py
+++ tests/sdc_scissor/testing_api/test_test_monitor.py
@@ -14,20 +14,23 @@ class TestTestMonitor:
         self.test_monitor = TestMonitor(simulator=None, test=None, oob=None, road_model=None)
         self.test_monitor.start_timer()
 
-    def test_is_car_moving_car_stays_on_same_position(self):
-        self.test_monitor.data = [(0, 0, 0, 0), (5, 0, 0, 0)]
+    def test_is_car_moving_car_stays_on_same_position(self, mocker):
+        self.test_monitor.test = mocker.patch('sdc_scissor.testing_api.test.Test')
+        self.test_monitor.test.simulation_data = [(0, 0, 0, 0), (5, 0, 0, 0)]
         expected = False
         actual = self.test_monitor.is_car_moving()
         assert expected == actual
 
-    def test_is_car_moving_car_stays_not_on_same_position(self):
-        self.test_monitor.data = [(0, 0, 0, 0), (5, 10, 10, 0)]
+    def test_is_car_moving_car_stays_not_on_same_position(self, mocker):
+        self.test_monitor.test = mocker.patch('sdc_scissor.testing_api.test.Test')
+        self.test_monitor.test.simulation_data = [(0, 0, 0, 0), (5, 10, 10, 0)]
         expected = True
         actual = self.test_monitor.is_car_moving()
         assert expected == actual
 
-    def test_is_car_moving_not_enough_sim_data_yet_to_make_decision(self):
-        self.test_monitor.data = [(0, 0, 0, 0), (0.1, 0, 0, 0)]
+    def test_is_car_moving_not_enough_sim_data_yet_to_make_decision(self, mocker):
+        self.test_monitor.test = mocker.patch('sdc_scissor.testing_api.test.Test')
+        self.test_monitor.test.simulation_data = [(0, 0, 0, 0), (0.1, 0, 0, 0)]
         expected = True
         actual = self.test_monitor.is_car_moving()
         assert expected == actual
