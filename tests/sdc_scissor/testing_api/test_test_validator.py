5d4fa37120e7605df09141d864e326372f7fe569
Christian Birchler <christian.birchler2@uzh.ch>
2022-09-22 18:36:27 +0200

    style: formatting
    refs: #73

--- tests/sdc_scissor/testing_api/test_test_validator.py
+++ tests/sdc_scissor/testing_api/test_test_validator.py
@@ -3,7 +3,7 @@ from sdc_scissor.testing_api.test_validator import SimpleTestValidator, NoInters
 
 class TestTestValidator:
     def test_simple_test_validator(self, mocker):
-        test = mocker.patch('sdc_scissor.testing_api.test_validator.Test')
+        test = mocker.patch("sdc_scissor.testing_api.test_validator.Test")
         tv = SimpleTestValidator()
         expected = True
         actual = tv.validate(test)
@@ -17,8 +17,12 @@ class TestTestValidator:
         assert actual == expected
 
     def test_no_intersection_decorator_on_invalid_test(self):
-        test = Test(0, road_points=[[10, 10], [100, 10], [150, 50], [100, 100], [60, 60], [20, 0], [20, -10]],
-                    test_outcome=None, test_duration=None)
+        test = Test(
+            0,
+            road_points=[[10, 10], [100, 10], [150, 50], [100, 100], [60, 60], [20, 0], [20, -10]],
+            test_outcome=None,
+            test_duration=None,
+        )
         tv = NoIntersectionValidator(SimpleTestValidator())
         expected = False
         actual = tv.validate(test)
