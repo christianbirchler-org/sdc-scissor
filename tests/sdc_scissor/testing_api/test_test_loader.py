be51ef7714aa4392f5cd7335e536debeda71eada
Christian Birchler <christian.birchler2@uzh.ch>
2022-09-23 11:46:53 +0200

    test: adapt to decorator pattern
    refs: #73

--- tests/sdc_scissor/testing_api/test_test_loader.py
+++ tests/sdc_scissor/testing_api/test_test_loader.py
@@ -3,20 +3,20 @@ import pathlib
 import pytest
 
 from sdc_scissor.testing_api.test_loader import TestLoader
-from sdc_scissor.testing_api.test_validator import NoIntersectionValidator
+from sdc_scissor.testing_api.test_validator import SimpleTestValidator, NoIntersectionValidator
 
 
 class TestTestLoader:
     def test_empty_test_folder_should_have_no_next_test(self, mocker):
         empty_test_dir_mock = mocker.patch("pathlib.Path")
-        tv = NoIntersectionValidator()
+        tv = NoIntersectionValidator(SimpleTestValidator())
         test_loader = TestLoader(tests_dir=empty_test_dir_mock, test_validator=tv)
         has_next = test_loader.has_next()
         assert not has_next
 
     def test_empty_test_folder_should_have_no_next_test_and_throw_exception_on_next(self, mocker):
         empty_test_dir_mock = mocker.patch("pathlib.Path")
-        tv = NoIntersectionValidator()
+        tv = NoIntersectionValidator(SimpleTestValidator())
         test_loader = TestLoader(tests_dir=empty_test_dir_mock, test_validator=tv)
         with pytest.raises(Exception):
             _ = test_loader.next()
@@ -28,7 +28,7 @@ class TestTestLoader:
         fs.create_file(r"./tests/no-match.json")
         fs.create_file(r"./tests/unlabeled/test.json")
         non_empty_test_dir_mock = pathlib.Path("./tests")
-        tv = NoIntersectionValidator()
+        tv = NoIntersectionValidator(SimpleTestValidator())
         test_loader = TestLoader(tests_dir=non_empty_test_dir_mock, test_validator=tv)
         test_loader.test_paths = [mocker.stub(), mocker.stub()]
         has_next = test_loader.has_next()
