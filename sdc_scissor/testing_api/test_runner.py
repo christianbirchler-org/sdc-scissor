fix: rotation calculation
refs: #83

sdc_scissor/testing_api/test_runner.py
@@ -1,3 +1,4 @@
+import math
 import time
 import logging
 
@@ -14,7 +15,6 @@ from sdc_scissor.testing_api.road_model import RoadModel
 from sdc_scissor.simulator_api.abstract_simulator import AbstractSimulator
 from sdc_scissor.obstacle_api.obstacle_factory import ObstacleFactory
 from sdc_scissor.testing_api.test_validator import TestIsNotValidException
-from sdc_scissor.simulator_api.beamng_simulator import compute_quaternion_rotation
 
 
 def _define_obstacles(road_model, obstacle_factory, bump_dist, delineator_dist, tree_dist) -> list:
@@ -31,12 +31,18 @@ def _define_obstacles(road_model, obstacle_factory, bump_dist, delineator_dist, 
             bump.x_pos = point.x
             bump.y_pos = point.y
             bump.z_pos = -28.0
-            # TODO: compute orientation
-            point_t1 = road_model.ideal_trajectory.interpolate(-current_distance + 1)
-            dir_vec = np.array([point_t1.x - point.x, point_t1.y - point.y])
-            z_euler_rotation = compute_quaternion_rotation(dir_vec)
-            # bump.rot_quat = tuple(Rotation.from_euler("zyx", [z_euler_rotation, 0, 0], degrees=False).as_quat())
-            # bump.rot_quat = z_euler_rotation
+
+            point_t1 = road_model.ideal_trajectory.interpolate(-current_distance - 1)
+            direction_vector = np.array([point_t1.x - point.x, point_t1.y - point.y])
+
+            ey = [0, 1]
+            dot_product = np.inner(ey, direction_vector)
+            determinant = ey[0] * direction_vector[1] - direction_vector[0] * ey[1]
+            theta = -math.atan2(determinant, dot_product)
+
+            rotation = Rotation.from_rotvec(theta * np.array([0, 0, 1]))
+            bump.rot_quat = tuple(rotation.as_quat())
+
             obstacles_lst.append(bump)
 
     if delineator_dist: