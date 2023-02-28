1b77cbe8f77eebf301f5285797f26680c3bab37e
Christian Birchler <christian.birchler2@uzh.ch>
2023-02-28 11:23:30 +0100

    docs: automatically build uml

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "SDC-Scissor"
-copyright = "2022, Christian Birchler"
+copyright = "2022-2023, Christian Birchler"
 author = "Christian Birchler"
 
 # The full version, including alpha/beta/rc tags
